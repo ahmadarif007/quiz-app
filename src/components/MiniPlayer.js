b8255e303cc98977d94f9826540cb90e4cd62d89
mdariful.islam <mdariful.islam@bracits.com>
2022-06-23 13:06:54 +0600

    Arif | v.6-result | update miniplayer deign

--- src/components/MiniPlayer.js
+++ src/components/MiniPlayer.js
@@ -3,7 +3,7 @@ import classes from "../styles/MiniPlayer.module.css";
 
 export default function MiniPlayer() {
   return (
-    <div className={`${classes.miniPlayer}, ${classes.floatingBtn}`}>
+    <div className={`${classes.miniPlayer} ${classes.floatingBtn}`}>
       <span className={`material-icons-outlined ${classes.open}`}>
         {" "}
         play_circle_filled{" "}
--- src/components/pages/Quiz.js
+++ src/components/pages/Quiz.js
@@ -15,8 +15,8 @@ export default function Quiz() {
       <Answers />
       <Answers />
       <Answers />
-      <MiniPlayer />
       <ProgressBar />
+      <MiniPlayer />
     </>
   );
 }
