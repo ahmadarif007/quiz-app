227b881c7c4ca7f5c262f8a6cea3715ee664cb7a
mdariful.islam <mdariful.islam@bracits.com>
2022-06-23 18:06:18 +0600

    Arif | v.7-routing | routing complete for home page

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,26 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "../styles/App.css";
 import Layout from "./Layout";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Quiz from "./pages/Quiz";
 import Result from "./pages/Result";
-// import Quiz from "./pages/Quiz";
-// import Home from "./pages/Home";
-// import SignUp from "./pages/SignUp";
+import SignUp from "./pages/SignUp";
 
 function App() {
   return (
     <div className="App">
-      <Layout>
-        {/* <Home /> */}
-        {/* <SignUp /> */}
-        {/* <Login /> */}
-        {/* <Quiz /> */}
-        <Result />
-      </Layout>
+      <Router>
+        <Layout>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="signUp" element={<SignUp />} />
+            <Route exact path="login" element={<Login />} />
+            <Route exact path="quiz" element={<Quiz />} />
+            <Route exact path="result" element={<Result />} />
+          </Routes>
+        </Layout>
+      </Router>
     </div>
   );
 }
